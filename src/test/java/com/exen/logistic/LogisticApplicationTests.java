69102aad7f4fc92c3eb278b79ad51cc811b251ad
exen <kirill118reg@gmail.com>
2021-06-21 23:15:41 +0400

    Application initialization. Configuring the frontend. Installing Webix, REQUIREJS, Routie.

---
+++ src/test/java/com/exen/logistic/LogisticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exen.logistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
